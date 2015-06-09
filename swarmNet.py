458d33105aa6784b202adf878b6c96e84037d595
willemneal <willem.neal@gmail.com>
2015-06-09 18:27:46 -0400

    added animation to see points move around

--- swarmNet.py
+++ swarmNet.py
@@ -1,10 +1,14 @@
-from random import random
-from math import pi, cos, sin
+from random import random, uniform
+from math import pi, cos, sin, sqrt
+import networkx as nx
 # -- t = 2*pi*random()
 # -- u = random()+random()
 # -- r = if u>1 then 2-u else u
 # -- [r*cos(t), r*sin(t)]
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+WALKERS = 100
 
 class Walker:
     def __init__(self):
@@ -13,13 +17,45 @@ class Walker:
         self.t = 2*pi*random()
         self.x =  self.r*cos(self.t)
         self.y =  self.r*sin(self.t)
+        self.Vin = 1./100.
+        self.Vint= uniform(0,2*pi)
 
-walkers = [Walker() for i in range(1000)]
+    def next(self):
+        newAngle = uniform(0,2*pi)
+        self.x += self.Vin*cos(self.Vint + newAngle)
+        self.y += self.Vin*sin(self.Vint + newAngle)
 
-def plotWalkers():
-    plt.scatter([walker.x for walker in walkers],
-            [walker.y for walker in walkers])
 
-    plt.show()
+walkers = [Walker() for i in range(WALKERS)]
+fig = plt.figure()
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+ax = fig.add_subplot(111, aspect='equal', autoscale_on=False,
+                     xlim=(-1, 1), ylim=(-1, 1))
+walkersX = [walker.x for walker in walkers]
+walkersY = [walker.y for walker in walkers]
 
-plotWalkers()
+walkersPlot, = ax.plot(walkersX, walkersY, 'bo',ms=6)
+
+G = nx.Graph()
+
+G.add_nodes_from(walkers)
+
+unitConnectionRadius = sqrt(1./len(walkers))
+
+
+def updateGraph(g):
+    pass
+
+
+def animate(i):
+    global walkers,walkersPlot
+
+    walkersPlot.set_data([walker.x for walker in walkers],
+                         [walker.y for walker in walkers])
+    map(lambda x: x.next(),walkers)
+    return walkersPlot
+
+ani = animation.FuncAnimation(fig, animate, frames=6000,
+                              interval=100)
+#ani.save('walkers.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+plt.show()
