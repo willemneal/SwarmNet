ef54625da297925fd4690847e99b8cca2dfa63d6
willemneal <willem.neal@gmail.com>
2015-06-10 13:15:31 -0400

    Made things more object oriented and added KDtree to help search for nearby points.

--- swarmNet.py
+++ swarmNet.py
@@ -10,12 +10,12 @@ from operator import attrgetter
 # -- [r*cos(t), r*sin(t)]
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from scipy import spatial as sp
+WALKERS = 100
 
-WALKERS = 1000
-unitConnectionRadius = sqrt(1./WALKERS)
 class Walker:
     number=0
-    def __init__(self):
+    def __init__(self,*args,**kwargs):
         self.num = Walker.number
         Walker.number +=1
         u = random()+random()
@@ -40,55 +40,105 @@ class Walker:
     def __repr__(self):
         return "(%.4f,%.4f)"%(self.x,self.y)
 
-walkers = [Walker() for i in range(WALKERS)]
-fig = plt.figure()
-fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
-ax = fig.add_subplot(111, aspect='equal', autoscale_on=False,
-                     xlim=(-1, 1), ylim=(-1, 1))
-walkersX = [walker.x for walker in walkers]
-walkersY = [walker.y for walker in walkers]
+    def location(self):
+        return (self.x,self.y)
 
-walkersPlot, = ax.plot(walkersX, walkersY, 'bo',ms=2)
+    @staticmethod
+    def createSwarm(number, *args, **kwargs):
+        '''
+        Little note: one would think that[Walker()]*5
+            would work, but this copies the same pointer to the object
+            However, in the case of arguments this is not a problem.
 
-G = nx.Graph()
-
-G.add_nodes_from(walkers)
-
-
+        '''
+        return map(Walker, [ (args, kwargs) ] * number)
+        return [Walker(args,kwargs) for i in range(number)]
 
 
-def updateGraph():
-    global G,walkers
-    sortedWalkers = sorted(walkers,key=attrgetter('x','y'))
-    print sortedWalkers
+
+class KDTree(sp.kdtree.KDTree):
+    def __init__(self,walkers):
+        self.walkers = walkers
+        super(KDTree,self).__init__([walker.location() for walker in walkers])
+
+    def getEdges(self, radius):
+        results = []
+        for i,neighbors in enumerate(self.query_ball_tree(self,radius)):
+            if len(neighbors) > 0:
+                for n in neighbors:
+                    results.append((self.walkers[i],self.walkers[n]))
+        return results
 
 
-    for walker in walkers:
-        neighbors = [w for w in walkers if w!=walker
-                    and w.bumped(walker)]
-        G.add_edges_from(zip([walker]*len(neighbors),neighbors))
+class Swarm(list):
+    record = False
+    def  __init__(self, *args, **kwargs):
+        self.kdTree = None
+        self.dt = 1
+        self.G = nx.Graph()
 
 
-def timeStep(i):
-    global walkers,walkersPlot
+    def createNew(self, numNodes):
+        self.extend(Walker.createSwarm(numNodes))
+        self.G.add_nodes_from(self)
+        self.unitConnectionRadius = sqrt(1./numNodes)
 
-    walkersPlot.set_data([walker.x for walker in walkers],
-                         [walker.y for walker in walkers])
-    map(lambda x: x.next(),walkers)
-    updateGraph()
-    return walkersPlot
 
-# ani = animation.FuncAnimation(fig, timeStep, frames=60,
-#                               interval=100)
-# #ani.save('walkers.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
-# plt.show()
-for i in range(100):
-    timeStep(i)
+    def getSwarm(self):
+        ''' list of walkers'''
+        return self
 
-def plotGraphConnections(graph):
-    counts = Counter(graph.degree().values())
-    plt.figure()
-    plt.bar(counts.keys(),counts.values())
-    plt.show()
+    def timeStep(self,steps=1):
+        self.initframe()
+        self.updateGraph()
 
-plotGraphConnections(G)
+    def initframe(self):
+        self.setPlotData()
+        map(lambda x: x.next(),self)
+
+    def buildTree(self):
+        ''' Kdtree to search for neighbors '''
+        self.kdTree = KDTree(self)
+
+    def updateGraph(self):
+        self.buildTree()
+        self.G.add_edges_from(self.kdTree.getEdges(self.unitConnectionRadius))
+
+    def getWalkersLocation(self):
+        return [walker.location() for walker in self]
+
+    def record(self):
+        self.recordWalkers()
+
+    def setPlotData(self):
+        self.walkersPlot.set_data(self.getWalkersX(),self.getWalkersY())
+
+    def getWalkersX(self):
+        return [walker.x for walker in self]
+
+    def getWalkersY(self):
+        return [walker.y for walker in self]
+
+    def initRecord(self):
+        self.fig = plt.figure()
+        self.fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+        self.ax = self.fig.add_subplot(111, aspect='equal', autoscale_on=False,
+                             xlim=(-1, 1), ylim=(-1, 1))
+        self.walkersPlot, = self.ax.plot(self.getWalkersX(), self.getWalkersY(), 'bo',ms=2)
+
+    def recordWalkers(self, frames = 60, interval = 100):
+        self.initRecord()
+        ani = animation.FuncAnimation(self.fig, self.timeStep, frames=60,
+                                      interval=100)
+        ani.save('walkers-%5d.mp4'%(len(self)), fps=30, extra_args=['-vcodec', 'libx264'])
+        plt.show()
+
+    def plotGraphConnections(self):
+        counts = Counter(self.G.degree().values())
+        plt.figure()
+        plt.bar(counts.keys(),counts.values())
+        plt.show()
+
+S = Swarm()
+S.createNew(100)
+S.recordWalkers()
